88ffe025433d85e080e48eade9278f864c6ca8c3
Gustaf franzèn <gustaffranzen@icloud.com>
2022-11-17 21:15:42 +0100

    added more function typedefs

--- src/function.h
+++ src/function.h
@@ -11,9 +11,11 @@
 
 #include "types.h"
 
+typedef u64_t (*hash_f)(const void *a, u64_t size);
 typedef i32_t (*compare_f)(const void *a, const void *b);
 typedef bool  (*predicate_f)(const void *arg);
 typedef void  (*action_f)(const void *arg);
 typedef void  (*noreturn_f)(void *arg);
 typedef void  *(*returning_f)(const void *arg);
+typedef void   (*tostring_f)(const void *arg, char *res);
 #endif /* _UL_FUNCTION_H_ */
